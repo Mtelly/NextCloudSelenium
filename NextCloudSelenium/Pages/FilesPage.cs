f48382d0f7f0950703bf301b649aa0f54a09f36c
dennisaguilar1989@gmail.com <dennisaguilar1989@gmail.com>
2025-06-24 21:33:19 -0500

    Created POM for Files page. Created new test for creating a folder (WIP).

---
+++ NextCloudSelenium/Pages/FilesPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextCloudSelenium.Pages
+{
+    public class FilesPage
+    {
+        private readonly IWebDriver _driver;
+
+        public FilesPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+                
+        private IWebElement NewButton => _driver.FindElement(By.ClassName("breadcrumb__actions"));
+        private IWebElement NewFolderButton => _driver.FindElement(By.CssSelector("[data-cy-upload-picker-menu-entry='newFolder']"));
+        private IWebElement FolderNameTextBox => _driver.FindElement(By.Id("inputassmv"));
+        private IWebElement CreateNewFolderButton => _driver.FindElement(By.CssSelector("[data-cy-files-new-node-dialog-submit]"));
+
+        public void CreateNewFolder(string folderName = "Selenium Test")
+        {
+            //Drop-down menu
+            NewButton.Click();
+            NewFolderButton.Click();
+
+            //Popup modal
+            FolderNameTextBox.Click();
+            FolderNameTextBox.Clear();
+            FolderNameTextBox.SendKeys(folderName);
+            CreateNewFolderButton.Click();
+        }
+    }
+}
---
+++ NextCloudSelenium/Tests/UpgradeTest.cs
@@ -0,0 +1,48 @@
+﻿using DotNetEnv;
+using NextCloudSelenium.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextCloudSelenium.Tests
+{
+    [TestFixture]
+    public class UpgradeTest
+    {
+        IWebDriver driver;
+        NavigationBar navigationPage;
+        FilesPage filesPage;
+        LoginPage loginPage;
+        DateTime now;
+        string url = string.Empty, username = string.Empty, pswd = string.Empty;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver();
+            filesPage = new FilesPage(driver);
+            navigationPage = new NavigationBar(driver);
+            Env.TraversePath().Load();
+            url = Env.GetString("ENVIRONMENT");
+            username = Env.GetString("USERNAME");
+            pswd = Env.GetString("MY_PASSWORD");
+            driver.Navigate().GoToUrl(url);
+            loginPage = new LoginPage(driver);
+            loginPage.LoginAs(username, pswd);
+        }
+
+        [Test]
+        public void TalkTest()
+        {
+            navigationPage.ClickFiles();
+            filesPage.CreateNewFolder();
+            IWebElement cyFiles = driver.FindElement(By.CssSelector("[data-cy-files-list-row-name=\"Documents\"]"));
+            Assert.That(driver.Title, Is.EqualTo("Talk - Nextcloud"));
+        }
+    }
+}
